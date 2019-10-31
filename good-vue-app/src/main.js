e2adaf486b5f1661be91ac1a7fbb90ca43da2d75
q-jason <596908546@qq.com>
2019-10-31 19:33:11 +0800

    no message

--- good-vue-app/src/main.js
+++ good-vue-app/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 // codesandbox 会报错
 // import './components'
@@ -20,6 +19,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
