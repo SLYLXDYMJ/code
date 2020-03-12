d1e48307169e55906e85f10e531bd8415d8bad13
q-jason <596908546@qq.com>
2020-03-12 11:18:39 +0800

    no message

--- vue-cli/src/plugins/vuex.js
+++ vue-cli/src/plugins/vuex.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import { mapState, mapGetters, mapActions, mapMutations } from 'vuex'
+import _ from 'lodash'
 
+import store from '../store'
+
+/**
+ *  将 store 中的 state getter action mutations
+ *  全部注入 Vue 实例中，就不用了在组件中单独引入了
+ **/
 Vue.mixin({
   computed: {
-    ...mapState([]),
-    ...mapGetters([])
+    ...mapState(_.keys(store.state)),
+    ...mapGetters(_.keys(store.getters))
   },
   methods: {
-    ...mapActions([]),
-    ...mapMutations([])
+    ...mapMutations(_.keys(store._mutations)),
+    ...mapActions(_.keys(store._actions))
   }
 })
